417749dbc0edd4479a232102aaac57f01b1b096d
Bruce Xu <ganyu.x@leantaas.com>
2020-11-07 16:15:39 -0800

    Experimental test case

--- scratch.py
+++ scratch.py
@@ -2,6 +2,7 @@ import datetime as dt
 
 from chives import Order, Transaction, engine as sql_engine
 from chives.matchingengine import MatchingEngine
+import pika
 
 ask_1 = Order(security_symbol="AAPL", side="ask", size=100, price=100)
 ask_2 = Order(security_symbol="AAPL", side="ask", size=100, price=99)
@@ -9,9 +10,20 @@ bid_1 = Order(security_symbol="AAPL", side="bid", size=120, price=101)
 apple_engine = MatchingEngine("AAPL", sql_engine)
 
 if __name__ == "__main__":
-    apple_engine.heartbeat(debug=True, incoming=ask_1)
-    apple_engine.heartbeat(debug=True, incoming=ask_2)
-    apple_engine.heartbeat(debug=True, incoming=bid_1)
+    msgs = [
+        ask_1.to_json(),
+        ask_2.to_json(),
+        bid_1.to_json(),
+    ]
+    
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='AAPL')
 
-    print(apple_engine.session.query(Transaction).all())
-    print(apple_engine.session.query(Order).all())
+    for msg in msgs:
+        channel.basic_publish(exchange='', routing_key='AAPL', body=msg)
+    connection.close()
+
+    print(apple_engine.session.query(Order).count())
+    print(apple_engine.session.query(Transaction).count())
