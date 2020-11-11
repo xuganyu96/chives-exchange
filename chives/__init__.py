ded85f8647d0ad16c9aa9af125dcebe1fa9a76ea
Ganyu Xu <xuganyu@berkeley.edu>
2020-11-10 20:22:45 -0800

    Added submit_order route

--- chives/__init__.py
+++ chives/__init__.py
@@ -1,12 +1,12 @@
 import os 
 
-from flask import Flask 
+from flask import Flask, request, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from chives.models import Base, Order, Transaction
 
-DATABASE = "/tmp/sqlite.db"
+DATABASE = "/tmp/sqlite.db" 
 engine = create_engine(f"sqlite:///{DATABASE}", echo=True)
 Base.metadata.create_all(engine)
 
@@ -36,8 +36,33 @@ def create_app(test_config = None) -> Flask:
     except OSError:
         pass
 
-    @app.route("/hello")
+    @app.route("/debug/can_connect")
     def hello():
-        return "Hello World"
+        return "You are connected to chives exchange"
+    
+    @app.route("/api/submit_order", methods=("GET", "POST"))
+    def submit_order():
+        if request.method == "POST":
+            # TODO: implement this POST route such that it accepts a JSON object 
+            # that can be parsed into an Order object. Once the order object 
+            # is validated, send it into the order queue.
+            incoming_json = request.data
+            incoming_order = Order.from_json(incoming_json)
+            print(incoming_order)
+            return incoming_json
+        else:
+            sample_order = Order()
+            return jsonify({
+                'order_id': "Integer",
+                'security_symbol': "String",
+                'side': "Either ask or bid",
+                'size': "Positive integer",
+                'price': "Positive float or null",
+                'all_or_none': "true or false",
+                'immediate_or_cancel': "true or false",
+                'active': "true or false",
+                'parent_order_id': "Integer or null",
+                'cancelled_dttm': "null"
+            })
     
     return app
