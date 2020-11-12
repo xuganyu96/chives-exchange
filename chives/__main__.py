b8a4292495a1de8d5f5d03f12aa1e96c3cbacd62
Ganyu Xu <xuganyu@berkeley.edu>
2020-11-11 21:47:43 -0800

    Consolidated subcommand actions

--- chives/__main__.py
+++ chives/__main__.py
@@ -8,11 +8,8 @@ if __name__ == "__main__":
     args = chives_parser.parse_args()
 
     if args.subcommand == "start_engine":
-        security_symbol = args.security_symbol
-        queue_host = args.queue_host
-        sql_uri = args.sql_uri
-        sql_engine = create_engine(sql_uri)
-        me_main(queue_host, sql_engine, security_symbol)
+        sql_engine = create_engine(args.sql_uri, echo=args.verbose)
+        me_main(args.queue_host, sql_engine, args.security_symbol)
     if args.subcommand == "initdb":
         sql_engine = create_engine(args.sql_uri, echo=args.verbose)
         Base.metadata.create_all(sql_engine)
