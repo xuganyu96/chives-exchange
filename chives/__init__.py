601acad19e6fe0759d425877803053e45bc6623d
Ganyu Xu <xuganyu@berkeley.edu>
2020-11-15 15:33:19 -0800

    Removed the manually implemented order submission route because we will implemented with blueprints later

--- chives/__init__.py
+++ chives/__init__.py
@@ -43,34 +43,4 @@ def create_app(test_config = None) -> Flask:
     def hello():
         return "You are connected to chives exchange"
     
-    @app.route("/api/submit_order", methods=("GET", "POST"))
-    def submit_order():
-        if request.method == "POST":
-            incoming_json = request.data
-            incoming_order = Order.from_json(incoming_json)
-            if incoming_order.security_symbol not in g.matching_engines:
-                ob_engine = create_engine(
-                    f"sqlite:////tmp/chives_{ob_engine}.ob.sqlite")
-                with app.app_context():
-                    g.matching_engines[incoming_order.security_symbol] = ME(
-                        incoming_order.security_symbol,
-                        me_engine, ob_engine
-                    )
-            me: ME = g.matching_engines[incoming_order.security_symbol]
-            me.heartbeat(incoming_order)
-            return incoming_json
-        else:
-            return jsonify({
-                'order_id': "Integer",
-                'security_symbol': "String",
-                'side': "Either ask or bid",
-                'size': "Positive integer",
-                'price': "Positive float or null",
-                'all_or_none': "true or false",
-                'immediate_or_cancel': "true or false",
-                'active': "true or false",
-                'parent_order_id': "Integer or null",
-                'cancelled_dttm': "null"
-            })
-    
     return app
