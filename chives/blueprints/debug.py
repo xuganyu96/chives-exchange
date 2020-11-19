c44916cb30f3692f3f20132914c7136008ede39c
Ganyu Xu <xuganyu@berkeley.edu>
2020-11-18 20:35:51 -0800

    Changed can_connect to is_authenticated for better organization

--- chives/blueprints/debug.py
+++ chives/blueprints/debug.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 bp = Blueprint("debug", __name__, url_prefix="/debug")
 
-@bp.route("/can_connect", methods=("GET",))
+@bp.route("/is_authenticated", methods=("GET",))
 @login_required
-def can_connect():
-    return "You are connected to chives exchange"
+def is_authenticated():
+    return f"You are authenticated as {current_user.username}"
