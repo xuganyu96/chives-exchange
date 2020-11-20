f62d4504e083bbbe37b0baa76b497e263251d29a
Ganyu Xu <xuganyu@berkeley.edu>
2020-11-19 23:21:08 -0800

    Added demo README

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # chives-exchange
 Simulated stock exchange
 
+## Run RabbitMQ with a single MatchingEngine
+First, run the RabbitMQ container:
+
+```bash
+docker run --rm \
+    --name "rabbitmq" \
+    -p 5672:5672 \
+    -p 15672:15672 \
+    rabbitmq:3-management
+```
+
+Second run matching engine
+
+Finally, run the test script to see if the system works.
+
 ## Run the flask application:
 ```bash
 export FLASK_APP=chives
--- scratch.py
+++ scratch.py
@@ -6,28 +6,29 @@ from sqlalchemy import create_engine
 import pika
 
 from chives.models import Order, Transaction
-from chives.matchingengine import MatchingEngine
+from chives.matchingengine.matchingengine import MatchingEngine
 
 ask_1 = Order(security_symbol="AAPL", side="ask", size=100, price=100)
 ask_2 = Order(security_symbol="AAPL", side="ask", size=100, price=99)
 bid_1 = Order(security_symbol="AAPL", side="bid", size=120, price=101)
-sql_engine = create_engine("sqlite:////tmp/sqlite.db", echo=False)
-apple_engine = MatchingEngine("AAPL", sql_engine)
+sql_engine = create_engine("sqlite:////tmp/chives.sqlite", echo=False)
+apple_engine = MatchingEngine(sql_engine)
 
 if __name__ == "__main__":
     msgs = [
-        json.dumps({'msg_type': 'incoming_order', 'msg_content': ask_1.json}),
-        json.dumps({'msg_type': 'incoming_order', 'msg_content': ask_2.json}),
-        json.dumps({'msg_type': 'incoming_order', 'msg_content': bid_1.json}),
+        ask_1.json,
+        ask_2.json,
+        bid_1.json,
     ]
     
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
-    channel.queue_declare(queue='AAPL')
+    channel.queue_declare(queue='incoming_order')
 
     for msg in msgs:
-        channel.basic_publish(exchange='', routing_key='AAPL', body=msg)
+        channel.basic_publish(
+            exchange='', routing_key='incoming_order', body=msg)
     connection.close()
 
     print("Wait for 1 second!")
