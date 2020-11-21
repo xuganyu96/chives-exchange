52486dec65d4b3d10a6047ae16da8ad56be88f07
Ganyu Xu <xuganyu@berkeley.edu>
2020-11-21 15:42:12 -0800

    Implemented exchange/submit_order to submit orders to MQ

--- chives/blueprints/exchange.py
+++ chives/blueprints/exchange.py
@@ -3,8 +3,10 @@ from flask import (
     session as flask_session, url_for
 )
 from flask_login import login_required, current_user
+import pika
+from pika.exceptions import AMQPConnectionError
 
-from chives.db import get_db
+from chives.db import get_db, get_mq
 from chives.forms import OrderSubmitForm
 from chives.models.models import Order
 
@@ -18,6 +20,17 @@ def dashboard():
 @bp.route("/submit_order", methods=("GET", "POST"))
 @login_required
 def submit_order():
+    # Even before rendering the page, try to connect to RabbitMQ. If RabbitMQ 
+    # is not running, then redirect to an error page
+    mq = ch = None
+    try:
+        mq: pika.BlockingConnection = get_mq()
+        ch = mq.channel()
+        ch.queue_declare(queue='incoming_order')
+    except AMQPConnectionError as e:
+        error_msg = "Webserver failed to connect to order queue"
+        return redirect(url_for("exchange.error", error_msg=error_msg))
+
     form = OrderSubmitForm(request.form)
     if request.method == "POST" and form.validate_on_submit():
         new_order: Order = Order(
@@ -35,8 +48,16 @@ def submit_order():
         db = get_db()
         db.add(new_order)
         db.commit()
-
-        ## TODO: Submit the order's JSON into the queue
+        ch.basic_publish(
+                exchange='', routing_key='incoming_order', body=new_order.json)
 
         return redirect(url_for("exchange.dashboard"))
     return render_template("exchange/submit_order.html", form=form)
+
+
+@bp.route("/error", methods=("GET",))
+def error():
+    error_msg = "Something went wrong..."
+    if 'error_msg' in request.args:
+        error_msg = request.args['error_msg']
+    return render_template("exchange/error.html", error_msg=error_msg)
