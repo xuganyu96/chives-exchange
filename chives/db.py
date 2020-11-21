a0db2a28b752bed3eb18dbc9184348ab7f012cec
Ganyu Xu <xuganyu@berkeley.edu>
2020-11-21 15:41:40 -0800

    Added RabbitMQ connection to flask global

--- chives/db.py
+++ chives/db.py
@@ -3,6 +3,7 @@ import os
 import click 
 from flask import current_app, g, _app_ctx_stack
 from flask.cli import with_appcontext
+import pika
 from sqlalchemy import create_engine 
 from sqlalchemy.ext.declarative import declarative_base 
 from sqlalchemy.orm import sessionmaker, scoped_session
@@ -29,6 +30,30 @@ def init_db():
     db = get_db()
     Base.metadata.create_all(engine)
 
+def get_mq() -> pika.BlockingConnection:
+    """If the webserver application has no active connection to the RabbitMQ 
+    server, then create one and append it to the flask G. Otherwise, return 
+    the connection on the flask G
+
+    :return: an active connection
+    :rtype: pika.BlockingConnection
+    """
+    if 'mq_conn' not in g:
+        g.mq_conn = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+    return g.mq_conn
+
+def close_mq(e=None):
+    """Close the MQ connection; this method will be added to teardown_appcontext
+
+    :param e: [description], defaults to None
+    :type e: [type], optional
+    """
+    mq_conn: pika.BlockingConnection = g.pop('mq_conn', None)
+    if mq_conn is not None:
+        mq_conn.close()
+
+
 @click.command("initdb")
 @with_appcontext
 def init_db_command():
@@ -37,4 +62,5 @@ def init_db_command():
 
 def init_app(app):
     app.teardown_appcontext(close_db)
+    app.teardown_appcontext(close_mq)
     app.cli.add_command(init_db_command)
