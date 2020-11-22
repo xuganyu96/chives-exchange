8e0b1ffa35f2ca7b3b3f1ffd41616604ebc412af
Ganyu Xu <xuganyu@berkeley.edu>
2020-11-21 17:33:43 -0800

    Added tools for injecting assets

--- scratch.py
+++ scratch.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 import pika
 
 from chives.db import SQLALCHEMY_URI
-from chives.models import Order, Transaction
+from chives.models.models import Order, Transaction, Asset, User
 from chives.matchingengine.matchingengine import MatchingEngine
 
 ask_1 = Order(security_symbol="AAPL", side="ask", size=100, price=100)
@@ -14,6 +14,21 @@ ask_2 = Order(security_symbol="AAPL", side="ask", size=100, price=99)
 bid_1 = Order(security_symbol="AAPL", side="bid", size=120, price=101)
 sql_engine = create_engine(SQLALCHEMY_URI, echo=False)
 apple_engine = MatchingEngine(sql_engine)
+
+def inject_asset(user_id, 
+    asset_symbol="AAPL", asset_amount=10, session=apple_engine.session):
+    """Given a user_id, insert a record of asset for this user
+
+    :param user_id: [description]
+    :type user_id: [type]
+    """
+    existing_asset = session.query(Asset).get((user_id, asset_symbol))
+    if existing_asset is None:
+        session.add(Asset(owner_id=user_id, 
+            asset_symbol=asset_symbol, asset_amount=asset_amount))
+    else:
+        existing_asset.asset_amount += asset_amount
+    session.commit()
 
 if __name__ == "__main__":
     msgs = [
