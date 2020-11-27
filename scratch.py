d84c9107a8a4f1b02ccbe7711a873aea080d93dc
Ganyu Xu <xuganyu@berkeley.edu>
2020-11-27 13:55:04 -0800

    Simulated trading

--- scratch.py
+++ scratch.py
@@ -1,23 +1,23 @@
 import datetime as dt
 import json
+import random
 import time
 
 from sqlalchemy import create_engine
 import pika
+from werkzeug.security import generate_password_hash 
 
 from chives.db import SQLALCHEMY_URI
 from chives.models.models import Order, Transaction, Asset, User, Company
 from chives.matchingengine.matchingengine import MatchingEngine
 
-ask_1 = Order(security_symbol="AAPL", side="ask", size=100, price=100)
-ask_2 = Order(security_symbol="AAPL", side="ask", size=100, price=99)
-bid_1 = Order(security_symbol="AAPL", side="bid", size=120, price=101)
 sql_engine = create_engine(SQLALCHEMY_URI, echo=False)
-apple_engine = MatchingEngine(sql_engine)
+me = MatchingEngine(sql_engine)
 
 def inject_asset(user_id, 
-    asset_symbol="AAPL", asset_amount=10, session=apple_engine.session):
-    """Given a user_id, insert a record of asset for this user
+        asset_symbol="AAPL", asset_amount=10, session=me.session):
+    """Given a user_id, insert a record of asset for this user, then return 
+    the Asset object
 
     :param user_id: [description]
     :type user_id: [type]
@@ -30,24 +30,70 @@ def inject_asset(user_id,
         existing_asset.asset_amount += asset_amount
     session.commit()
 
-if __name__ == "__main__":
-    msgs = [
-        ask_1.json,
-        ask_2.json,
-        bid_1.json,
-    ]
+    return session.query(Asset).get((user_id, asset_symbol))
+
+def inject_user(username: str, 
+        raw_password: str="password", session=me.session):
+    """Given a username and an optional raw_password, create a new user, add 
+    it to the database, and return the user object
+
+    :param username: [description]
+    :type username: str
+    :param raw_password: [description], defaults to "password"
+    :type raw_password: str, optional
+    """
+    user = session.query(User).filter(User.username == username).first()
+    if not user:  
+        new_user = User(username=username, 
+                        password_hash=generate_password_hash(raw_password))
+        session.add(new_user)
+        session.commit()
+        return new_user
+    else:
+        return user
+
+def simulate_trading(nrounds: int, symbol: str, matching_engine: MatchingEngine):
+    """Create 2 users: buyer and seller; create 1 company
+    For each round:
+    * Seller submits an ask of random size with random price
+    * Buyer submits a bid of equal size but at market price
+    * Sleep for randomly amount of seconds
+    """
+    seller = inject_user("seller", session=matching_engine.session)
+    buyer = inject_user("buyer", session=matching_engine.session)
+    inject_asset(seller.user_id, "_CASH", 10000000)
+    inject_asset(buyer.user_id, "_CASH", 10000000)
+
+    if not matching_engine.session.query(Company).get(symbol):
+        company = Company(
+            symbol=symbol,
+            name=symbol,
+            initial_value=10000,
+            initial_size=10,
+            founder_id=seller.user_id,
+            market_price=10000/10
+        )
+        matching_engine.session.add(company)
+        matching_engine.session.commit()
     
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue='incoming_order')
+    for r in range(nrounds):
+        random_size = random.randint(1, 100)
+        random_price = random.uniform(10, 100)
+        inject_asset(seller.user_id, symbol, random_size)
+        ask = Order(
+            security_symbol=symbol,
+            side="ask",
+            size=random_size,
+            price=random_price,
+            owner_id=seller.user_id
+        )
+        bid = Order(
+            security_symbol=symbol,
+            side="bid",
+            size=random_size,
+            price=None,
+            owner_id=buyer.user_id
+        )
+        matching_engine.heartbeat(ask)
+        matching_engine.heartbeat(bid)
 
-    for msg in msgs:
-        channel.basic_publish(
-            exchange='', routing_key='incoming_order', body=msg)
-    connection.close()
-
-    print("Wait for 1 second!")
-    time.sleep(1)
-    print(apple_engine.session.query(Order).count())
-    print(apple_engine.session.query(Transaction).count())
