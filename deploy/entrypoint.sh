d67c80dd8b81310642fb3d205559752386d402eb
Ganyu Xu <xuganyu@berkeley.edu>
2020-12-28 22:07:54 -0800

    First draft of shell entrypoint

---
+++ deploy/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo $1
+
+case "$1" in
+    webserver)
+        uwsgi --socket 127.0.0.1:5000 \
+              --wsgi-file chives_entrypoint.py \
+              --callable app \
+              --processes 4 \
+              --threads 2 \
+              --stats 127.0.0.1:5001
+        ;;
+    matchingengine)
+        python chives_entrypoint.py start_engine
+        ;;
+    *)
+    echo $1
+    ;;
+esac
