4107b75c6bf1e808d1940820e2eb0ecfcec5c459
Ganyu Xu <xuganyu@berkeley.edu>
2020-12-29 20:55:46 -0800

    implemented env overwtie

---
+++ chives/configs/default_config.py
@@ -0,0 +1,8 @@
+DEFAULT_CONFIG = {
+    "SQLALCHEMY_CONN": "sqlite:////tmp/chives.sqlite",
+    "RABBITMQ_HOST": "localhost",
+    "RABBITMQ_PORT": 5672,
+    "RABBITMQ_VHOST": "/",
+    "RABBITMQ_LOGIN": "guest",
+    "RABBITMQ_PASSWORD": "guest"
+}
