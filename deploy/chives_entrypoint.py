a03a89f657e6dd938b45419226343103a2c2abd2
Ganyu Xu <xuganyu@berkeley.edu>
2020-12-30 21:38:48 -0800

    All runtime configurations will be set using environment variables

--- deploy/chives_entrypoint.py
+++ deploy/chives_entrypoint.py
@@ -1,10 +1,10 @@
-"""Import the runtime configuration (as a Python dictionary) from 
-chives_config.py from the same directory as this file, then import create_app 
-from chives.webserver and create the Flask application object by passing in 
-the configuration object. 
+"""All of runtime configurations are set using environment variables. This is 
+okay because both create_app and start_engine will overwrite the default 
+configuration stored in chives.configs.DEFAULT_CONFIG using identically named 
+environment variables.
 
 In case the docker image is run with command "webserver", the shell entrypoint 
-script will call:
+script will call the equivalent of:
     uwsgi [OPTIONS] -w chives_entrypoint:app
 to run the application object
 
@@ -12,23 +12,16 @@ In case the docker image is run with command "matchingengine", the shell
 entrypoint will call:
     python chives_entrypoint.py start_engine
 which should invoke this python entrypoint's __main__ method, within which 
-chives.matchingengine.start_engine will be called with the configuration object 
-passed.
+chives.matchingengine.start_engine will be called.
 """
 import sys
 from sqlalchemy import create_engine
 from chives.webserver import create_app 
 from chives.matchingengine import start_engine 
 
-from chives_config import CONFIG
-
-
-# Adapt the global config to Flask application config
-flask_config = CONFIG.copy()
-flask_config['DATABASE_URI'] = flask_config['sql_alchemy_conn']
-app = create_app(CONFIG)
+# Flask application created for uwsgi 
+app = create_app()
 
 if __name__ == "__main__":
     if sys.argv[1] == "start_engine":
-        sql_engine = create_engine(CONFIG['sql_alchemy_conn'])
-        start_engine(CONFIG['rabbitmq_host'], sql_engine)
+        start_engine()
