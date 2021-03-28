28fcb09687e47a5b74b3454b4b835adae6efa165
Ganyu Xu <xuganyu@berkeley.edu>
2021-03-28 15:38:15 -0700

    Implemented password hashing and checking

--- chives/models/models.py
+++ chives/models/models.py
@@ -2,6 +2,7 @@ import datetime as dt
 
 from sqlalchemy import (Column, String, Integer, Time, DateTime)
 from sqlalchemy.ext.declarative import declarative_base
+from werkzeug.security import generate_password_hash, check_password_hash
 
 Base = declarative_base()
 
@@ -14,6 +15,18 @@ class Account(Base):
     name = Column(String(256), nullable=True)
     create_dttm = Column(DateTime, default=dt.datetime.utcnow)
     close_dttm = Column(DateTime, default=dt.datetime.utcnow)
+
+    @staticmethod 
+    def hash_password(password) -> str:
+        """Given a password, return the hash of a password using SHA256 
+        """
+        return generate_password_hash(password)
+
+    def check_password_hash(self, password) -> bool:
+        """Given a password, run werkzeug.check_password_hash against the hash
+        stored within the database"""
+        return check_password_hash(self.password_hash, password)
+
 
     def __str__(self) -> str:
         return f"<Account id={self.id} email={self.email}>"
